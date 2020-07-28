cdff92dfb8a6a9176f5d3cad4025aff6f8f05afa
PrathyushPRao <jay@prashy.com>
2020-07-29 01:41:59 +1000

    added bottom tab

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,25 +5,20 @@ import '../widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Deli Meals'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(10),
-        children: DUMMY_CATEGORIES
-            .map((e) => CategoryItem(
-                  e.id,
-                  e.title,
-                  e.color,
-                ))
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 1.5,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(10),
+      children: DUMMY_CATEGORIES
+          .map((e) => CategoryItem(
+                e.id,
+                e.title,
+                e.color,
+              ))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 1.5,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }

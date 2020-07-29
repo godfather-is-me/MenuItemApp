1256536f7e4dc8954cafb2d713ff604a49470bd2
PrathyushPRao <jay@prashy.com>
2020-07-29 22:33:14 +1000

    added drawer

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:menu_item_app/screens/category_meals_screen.dart';
 import 'screens/categories_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'screens/tabs_screen.dart';
+import 'screens/filter_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -33,6 +34,7 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FilterScreen.routeName: (ctx) => FilterScreen(),
       },
       /* onGenerateRoute: (settings) {
         print(settings.arguments);
